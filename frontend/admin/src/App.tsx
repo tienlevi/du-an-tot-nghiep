6ebf09aa4bfe2ea3fcc71a9de92104ac80bb4e20
TiennLevii <nguyentrachtien2401@gmail.com>
2024-05-20 15:39:55 +0700

    Đổi tên file

--- frontend/admin/src/App.tsx
+++ frontend/admin/src/App.tsx
@@ -6,7 +6,7 @@ import SignIn from './pages/Authentication/SignIn';
 import SignUp from './pages/Authentication/SignUp';
 import Calendar from './pages/Calendar';
 import Chart from './pages/Chart';
-import ECommerce from './pages/Dashboard/ECommerce';
+import ECommerce from './pages/Dashboard/Main';
 import Profile from './pages/Profile';
 import Settings from './pages/Settings';
 import Tables from './pages/Tables';
