e3c3d39953ee64629712a71d18baa836fbcee74b
TiennLevii <nguyentrachtien2401@gmail.com>
2024-06-05 21:06:32 +0700

    cắt giao diện client

---
+++ frontend/src/components/designLayouts/Flex.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Flex = ({ children, className }: any) => {
+  return <div className={className}>{children}</div>;
+};
+
+export default Flex;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 import './css/style.css';
 import './css/satoshi.css';
 import 'jsvectormap/dist/css/jsvectormap.css';
@@ -10,7 +13,11 @@ import 'flatpickr/dist/flatpickr.min.css';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </Router>
   </React.StrictMode>,
 );
