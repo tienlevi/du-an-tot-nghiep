ec8eb1237890e019a695a2ae23282ae384607489
TiennLevii <nguyentrachtien2401@gmail.com>
2024-07-13 20:39:03 +0700

    xóa giao diện phía website

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,49 +16,6 @@ import Tables from './pages/Tables';
 // Client
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import SpecialCase from './components/SpecialCase/SpecialCase';
-import Footer from './components/home/Footer/Footer';
-import FooterBottom from './components/home/Footer/FooterBottom';
-import Header from './components/home/Header/Header';
-import HeaderBottom from './components/home/Header/HeaderBottom';
-import About from './pages/About/About';
-import Cart from './pages/Cart/Cart';
-import Contact from './pages/Contact/Contact';
-import Home from './pages/Home/Home';
-import Journal from './pages/Journal/Journal';
-import Offer from './pages/Offer/Offer';
-import ProductDetails from './pages/ProductDetails/ProductDetails';
-import Shop from './pages/Shop/Shop';
-import Payment from './pages/payment/Payment';
-import Search from './pages/Search/Search';
-import Bill from './pages/Bill/Bill';
-
-const Layout = () => {
-  return (
-    <>
-      <ToastContainer
-        position="top-right"
-        autoClose={1000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-      />
-      <ToastContainer />
-
-      <Header />
-      <HeaderBottom />
-      <SpecialCase />
-      <Outlet />
-      <Footer />
-      <FooterBottom />
-    </>
-  );
-};
 
 function App() {
   const [loading, setLoading] = useState<boolean>(true);
@@ -192,21 +149,6 @@ function App() {
             </>
           }
         />
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/journal" element={<Journal />} />
-          <Route path="/category/:category" element={<Offer />} />
-          <Route path="/product/:_id" element={<ProductDetails />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/paymentgateway" element={<Payment />} />
-          <Route path="/bill" element={<Bill />} />
-        </Route>
-        <Route path="/signup" element={<SignUp />}></Route>
-        <Route path="/signin" element={<SignIn />}></Route>
       </Routes>
     </>
   );
