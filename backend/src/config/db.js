f50f1610245bded6d820e91bf24634b7ce0ed41c
TiennLevii <nguyentrachtien2401@gmail.com>
2024-09-26 10:12:17 +0700

    update: mongoose connect

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async (uri) => {
   try {
-    await mongoose.connect(uri);
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to db");
   } catch (error) {
     console.log(error);
