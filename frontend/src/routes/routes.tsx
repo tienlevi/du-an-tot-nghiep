b85d207a758809494394f2dda7729f83fc4972b1
thang 111 <thangnmt047@gmail.com>
2024-10-05 03:12:18 +0700

    category

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -36,7 +36,7 @@ import SearchProducts from '@/pages/(website)/Search';
 import UserList from '@/pages/(dashboard)/User/UserList';
 import UserAdd from '@/pages/(dashboard)/User/UserAdd';
 import UserEdit from '@/pages/(dashboard)/User/UserEdit';
-
+import Category from '@/pages/(website)/Category';
 
 function DashboardRoute() {
   return (
@@ -60,7 +60,7 @@ function DashboardRoute() {
                 <Route element={<Products />} path="/products" />
                 <Route element={<Product />} path="/allProducts/:title" />
                 <Route element={<SearchProducts />} path="/search" />
-                 {/* <Route element={<Category />} path="/category" />  */}
+                 <Route element={<Category />} path="/category" /> 
                 <Route element={<NotFound />} path="*" />
               </Route>
               <Route
