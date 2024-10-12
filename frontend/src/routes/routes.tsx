de79567d3f46b7011b55adaa423d68d9e8ecc2ca
vuonghaki2109 <nguyenhienvuong219@gmail.com>
2024-10-12 12:53:59 +0700

    Chi Tiet san pham

--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -54,7 +54,7 @@ function DashboardRoute() {
                 <Route element={<Checkout />} path="/checkout" />
                 <Route element={<Payment />} path="/payment" />
                 <Route element={<Products />} path="/products" />
-                <Route element={<Product />} path="/allProducts/:title" />
+                <Route element={<Product />} path="/products/:id" />
                 <Route element={<SearchProducts />} path="/search" />
                 {/* <Route element={<Category />} path="/category" />  */}
                 <Route element={<NotFound />} path="*" />
