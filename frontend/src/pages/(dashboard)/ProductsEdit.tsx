507148f490a7ebd9f93b884362586cd0696987bd
TiennLevii <nguyentrachtien2401@gmail.com>
2024-11-01 22:27:23 +0700

    Cập nhật trường attribute ở model product

--- frontend/src/pages/(dashboard)/ProductsEdit.tsx
+++ frontend/src/pages/(dashboard)/ProductsEdit.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { useNavigate, useParams } from 'react-router-dom';
@@ -8,6 +8,9 @@ import { editProduct, getProductById } from '@/services/product';
 import { getCategories } from '@/services/category';
 import UploadCloundinary from '@/utils/cloudinary';
 import DefaultLayout from './_components/Layout/DefaultLayout';
+import { Product } from '@/types/product';
+import { IoMdClose } from 'react-icons/io';
+import usePreview from '@/hooks/usePreview';
 
 interface Inputs {
   name: string;
@@ -18,20 +21,28 @@ interface Inputs {
   discount: number;
   countInStock: number;
   featured: boolean;
+  size: string;
 }
+
+const sizes = ['S', 'M', 'L', 'XL', '2XL'];
 
 const ProductsEdit = () => {
   const { id }: any = useParams();
   const element = useRef<HTMLInputElement>(null);
+  const elementColor = useRef<HTMLInputElement>(null);
   const {
     register,
     handleSubmit,
     formState: { errors },
     reset,
   } = useForm<Inputs>();
+  const imageFile = usePreview();
+  const imageFileColor = usePreview();
+  const [tagName, setTagName] = useState<string>('');
+  const [tags, setTags] = useState([]);
   const navigate = useNavigate();
-
-  const { data: product }: any = useQuery({
+  const checkbox = register('size');
+  const { data: product } = useQuery<Product>({
     queryKey: ['product', id],
     queryFn: async () => {
       const response = await getProductById(id);
@@ -67,13 +78,28 @@ const ProductsEdit = () => {
     },
   });
 
-  const onSubmit = (data: any) => {
-    mutate(data);
-  };
-
   useEffect(() => {
     reset(product);
   }, [product]);
+
+  const addTag = async (data: any) => {
+    const fileImageColor = elementColor.current?.files?.[0];
+    const image = await UploadCloundinary(fileImageColor);
+    const newTag = { name: tagName, image: image?.secure_url };
+    setTags([...tags, newTag] as any);
+    console.log(tags);
+  };
+
+  const removeTag = (id: number) => {
+    const remove = tags.filter((_, index) => index + 1 !== id);
+    setTags(remove);
+  };
+
+  const currentTags = [...(product?.attributes?.color || []), ...tags];
+
+  const onSubmit = (data: any) => {
+    mutate({ ...data, attributes: { size: data.size, color: currentTags } });
+  };
 
   return (
     <DefaultLayout>
@@ -95,11 +121,17 @@ const ProductsEdit = () => {
             <option value="" className="text-gray-500">
               Danh mục
             </option>
-            {categories?.data?.map((category: Category) => (
-              <option key={category._id} value={category._id}>
-                {category.name}
-              </option>
-            ))}
+            {categories?.data?.map((category: Category) =>
+              category._id === product?.category ? (
+                <option key={category._id} value={category._id} selected>
+                  {category.name}
+                </option>
+              ) : (
+                <option key={category._id} value={category._id}>
+                  {category.name}
+                </option>
+              ),
+            )}
           </select>
 
           <input
@@ -133,6 +165,68 @@ const ProductsEdit = () => {
             placeholder="Số lượng trong kho"
             className="w-full my-2 p-2 border border-gray-300 rounded"
           />
+          <div className="flex flex-col my-2 items-start">
+            <span className="mb-2">Size</span>
+            <div className="flex items-center">
+              {sizes?.map((size) => {
+                const isChecked = product?.attributes?.size?.includes(size);
+                return (
+                  <div key={size} className="flex items-center mr-2">
+                    <input
+                      type="checkbox"
+                      value={size}
+                      {...register('size')}
+                      className="mr-2"
+                      defaultChecked={isChecked}
+                    />
+                    <span>{size}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div>
+            <label htmlFor="">Màu sắc</label>
+            <div className="grid grid-cols-5 gap-2 w-full my-2 p-2 border border-gray-300 rounded">
+              {currentTags?.map((item: any, index) => (
+                <div
+                  key={index}
+                  className="flex items-center justify-center bg-blue-500 text-white py-2 rounded"
+                  onClick={() => removeTag(index + 1)}
+                >
+                  <p>{item.name}</p>
+                  <IoMdClose
+                    style={{
+                      color: 'white',
+                      marginLeft: 5,
+                      cursor: 'pointer',
+                    }}
+                  />
+                </div>
+              ))}
+            </div>
+            <label htmlFor="">Tên màu sắc</label>
+            <input
+              type="text"
+              value={tagName}
+              onChange={(e) => setTagName(e.target.value)}
+              placeholder="Tên Màu sắc"
+              className="w-full my-2 p-2 border border-gray-300 rounded"
+            />
+            <p>Ảnh Màu sắc</p>
+            <input
+              ref={elementColor}
+              type="file"
+              onChange={imageFileColor.handleChangeFile}
+              className="w-full my-2 p-2"
+            />
+            <div
+              onClick={addTag}
+              className="bg-red-500 w-[145px] text-white py-2 px-4 rounded hover:bg-red-600 cursor-pointer"
+            >
+              Thêm Màu sắc
+            </div>
+          </div>
           <div className="flex my-2 items-center">
             <input type="checkbox" {...register('featured')} className="mr-2" />
             <span>Featured</span>
