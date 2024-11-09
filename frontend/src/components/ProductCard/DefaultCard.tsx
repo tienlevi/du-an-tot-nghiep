3bb1a903c4bfbb616e05d4c486a8a314d47aaee4
vuongminh042 <vuongchiminh320@gmail.com>
2024-11-10 01:33:05 +0700

    feat: add to cart show drawer choose color

--- frontend/src/components/ProductCard/DefaultCard.tsx
+++ frontend/src/components/ProductCard/DefaultCard.tsx
@@ -1,9 +1,12 @@
 import { HeartOutlined } from '@ant-design/icons';
+import DrawerAddCart from '../DrawerAddCart';
+import { Rate } from 'antd';
+import { Currency } from '@/utils/FormatCurreny';
 
 export default function DefaultCard() {
   const discount = 2;
   return (
-    <div className="h-[432px] group cursor-pointer">
+    <div className="h-[452px] group cursor-pointer">
       <div className="w-[310px] h-[310px] relative">
         <img
           className="object-contain "
@@ -11,8 +14,10 @@ export default function DefaultCard() {
           alt=""
         />
         <div className="opacity-0 px-2 py-1 group-hover:opacity-100 flex items-center w-full justify-between duration-300 absolute bottom-0">
-          <button className="text-global hover:bg-hover duration-300 hover:text-white bg-white shadow-md flex justify-center w-3/4 bg- py-1.5 rounded-md text-sm font-medium">Thêm vào giỏ hàng</button>
-          <button className='w-1/6 h-[32px] bg-global hover:bg-opacity-80 duration-300 rounded-lg text-white'>
+          <DrawerAddCart classNameBtn="text-global hover:bg-hover px-10 duration-300 hover:text-white bg-white shadow-md flex justify-center w-full h-[32px] flex items-center justify-center rounded-md text-sm font-medium">
+            Thêm vào giỏ hàng
+          </DrawerAddCart>
+          <button className="w-1/6 h-[32px] bg-global hover:bg-opacity-80 duration-300 rounded-lg text-white">
             <HeartOutlined />
           </button>
         </div>
@@ -21,11 +26,12 @@ export default function DefaultCard() {
         <h3 className=" font-semibold group-hover:text-hover mt-4 w-[90%] text-ellipsis whitespace-nowrap overflow-hidden">
           Áo Thun Chạy Bộ Nam Adidas Own The Run
         </h3>
-        <p className="font-semibold mt-1">525.000đ</p>
+        <Rate allowHalf value={5} disabled className='text-xs'/>
+        <p className="font-semibold mt-1">{Currency(525000)}</p>
         {discount && (
           <div className="mt-1">
             <div className="flex gap-2 items-center">
-              <span className="line-through">750.000đ</span>
+              <span className="line-through">{Currency(750000)}</span>
               <span className="text-hover font-semibold">- 30%</span>
             </div>
             <div className="mt-2">
