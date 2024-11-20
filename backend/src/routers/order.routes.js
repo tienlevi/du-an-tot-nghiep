934a2939273d5780a23b853a518c6dc6ae0f2afa
Thang <thangnmt047@gmail.com>
2024-11-20 17:00:34 +0700

    feat(api-order): update minor

--- backend/src/routers/order.routes.js
+++ backend/src/routers/order.routes.js
@@ -5,10 +5,10 @@ import { authenticate } from "../middleware/authenticateMiddleware.js";
 const orderRouter = Router();
 orderRouter.post("/create", authenticate, orderControllers.createOrder);
 orderRouter.post("/canceled", authenticate, orderControllers.cancelOrder);
-orderRouter.post("/confirm", authenticate, orderControllers.confirmOrder);
-orderRouter.post("/shipping", authenticate, orderControllers.shippingOrder);
-orderRouter.post("/finish", authenticate, orderControllers.finishOrder);
-orderRouter.post("/deliver", authenticate, orderControllers.deliverOrder);
+orderRouter.patch("/confirm", authenticate, orderControllers.confirmOrder);
+orderRouter.patch("/shipping", authenticate, orderControllers.shippingOrder);
+orderRouter.patch("/done", authenticate, orderControllers.finishOrder);
+orderRouter.patch("/delivered", authenticate, orderControllers.deliverOrder);
 orderRouter.get("/my-order", authenticate, orderControllers.getAllOrdersByUser);
 orderRouter.get("/all", authenticate, orderControllers.getAllOrders);
 orderRouter.get(
